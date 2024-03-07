a8c61535cd38cde24321b734b9ccd53103c96e3e
karan <karan.suthar.lakha@gmail.com>
2024-03-07 21:45:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI("AIzaSyCScxkh3Jmm3miODBWLIDDEb0U4wierN88");
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+const readline =  require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+async function generateContent(prompt) {
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = await response.text();
+  console.log(text);
+  run();
+}
+function run(){
+  readline.question('Write a prompt: ', async (prompt) => {
+    await generateContent(prompt);
+  });
+}
+
+run();
