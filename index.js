97e0379b2609b2d5b501151e9c3007b385893dc2
karan <karan.suthar.lakha@gmail.com>
2024-03-08 14:28:57 +0530

    multi turn Chat input

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const gemini = require('./gemini.js');
+const gemini = require('./gemini.js')(process.env.GENERATIVE_API_KEY);
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/gemini', gemini.router);
+app.use('/gemini', gemini);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
